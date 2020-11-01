87892f61f1dd200ee3a863d56866f04958db07b9
Marc Boreu <34090642+marcboreu@users.noreply.github.com>
2020-11-02 00:05:48 +0100

    beta 1.1

---
+++ js/projects/projects.js
@@ -0,0 +1,27 @@
+
+/* Projects object */
+
+
+const projects = [
+    {
+    title: "Calculator",
+    class: "HTML-CSS Javascript",
+    imgSrc: "",
+    viewCode: "",
+    deployedCode: "",
+    },
+    {
+    title: "Calculator2",
+    class: "HTML-CSS Javascript",
+    imgSrc: "",
+    viewCode: "",
+    deployedCode: "",
+    },
+    {
+    title: "Calculator3",
+    class: "HTML-CSS Javascript",
+    imgSrc: "",
+    viewCode: "",
+    deployedCode: "",
+    },
+]
